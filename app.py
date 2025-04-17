d9bbac11dbdab253861b2191bedd9c79f4f13a6a
ramdaani <ramdanifauzi26@gmail.com>
2025-04-17 16:25:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.title("Halo, ini aplikasi streamlit pertama!")
-st.writer("aplikasi ini masih kosong, tapi berhasil jalan!")
+st.write("aplikasi ini masih kosong, tapi berhasil jalan!")
